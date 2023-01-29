ea6db3c0dfbcb395ec3c13c450f8d38e27654f33
Alex <17277800+alex-schaaf@users.noreply.github.com>
2023-01-29 22:38:46 +0100

    config: add ui-component plop action

--- plopfile.js
+++ plopfile.js
@@ -30,6 +30,37 @@ module.exports = plop => {
       },
     ],
   }),
+  plop.setGenerator('ui-component', {
+    description: 'Create a UI component',
+    // User input prompts provided as arguments to the template
+    prompts: [
+      {
+        // Raw text input
+        type: 'input',
+        // Variable name for this input
+        name: 'name',
+        // Prompt to display on command line
+        message: 'What is your component name?'
+      },
+    ],
+    actions: [
+      {
+        type: 'add',
+        path: 'packages/ui/components/{{name}}/{{name}}.tsx',
+        templateFile: 'plop-templates/Component/Component.tsx.hbs',
+      },
+      {
+        type: 'add',
+        path: 'packages/ui/components/{{name}}/{{name}}.test.tsx',
+        templateFile: 'plop-templates/Component/Component.test.tsx.hbs',
+      },
+      {
+        type: 'add',
+        path: 'packages/ui/components/{{name}}/index.ts',
+        templateFile: 'plop-templates/Component/index.ts.hbs',
+      },
+    ],
+  }),
   plop.setGenerator('service', {
     description: 'Create a service',
     // User input prompts provided as arguments to the template
