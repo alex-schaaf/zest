d9e3e6f6429b33489829d8e02152258522bd2058
Alex <17277800+alex-schaaf@users.noreply.github.com>
2023-01-29 16:55:56 +0100

    chore: make charts responsive

--- apps/frontend/src/components/ActivitiesBarChartContainer/ActivitiesBarChartContainer.tsx
+++ apps/frontend/src/components/ActivitiesBarChartContainer/ActivitiesBarChartContainer.tsx
@@ -3,29 +3,15 @@ import ActivitiesBarChart from "@/components/ActivitiesBarChart/ActivitiesBarCha
 import Card from "@/components/Card"
 import Loading from "@/components/Loading"
 import { StravaActivities } from "@prisma/client"
-import React, { useEffect, useRef, useState } from "react"
 import { ParentSize } from "@visx/responsive"
 
 const ActivitiesBarChartContainer: React.FC = () => {
-  const ref = useRef<any>(null)
-
-  const [{ width, height }, setDimensions] = useState({ width: 0, height: 0 })
-
   const start = new Date(new Date().getFullYear(), 0, 1)
   const { activities, isLoading, isError } = useActivities(
     start,
     undefined,
     !!start
   )
-
-  useEffect(() => {
-    if (ref.current) {
-      setDimensions({
-        width: ref.current.offsetWidth,
-        height: ref.current.offsetHeight,
-      })
-    }
-  }, [activities])
 
   if (isLoading) return <Loading />
   if (isError) return <div>fialed to fetch</div>
