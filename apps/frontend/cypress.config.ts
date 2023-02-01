aa98876d221746dddbbee174d3f9178014e7ef2f
Alex <17277800+alex-schaaf@users.noreply.github.com>
2023-02-01 21:12:25 +0100

    config: specify exact nodejs version

--- apps/frontend/cypress.config.ts
+++ apps/frontend/cypress.config.ts
@@ -1,8 +1,9 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from "cypress"
 
 export default defineConfig({
   e2e: {
     baseUrl: "http://localhost:5173",
+    video: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
@@ -14,4 +15,4 @@ export default defineConfig({
       bundler: "vite",
     },
   },
-});
+})
