7ba0701dcd03ec3d78dd4c42f54981d783bc9ee7
Alex <17277800+alex-schaaf@users.noreply.github.com>
2023-02-02 10:27:01 +0100

    config: e2e

--- apps/frontend/cypress/cypress.config.ts
+++ apps/frontend/cypress/cypress.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "cypress"
 
 export default defineConfig({
   e2e: {
-    baseUrl: "http://localhost:5173",
+    baseUrl: "http://localhost:4173",
     video: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
