4f7ab0363fb066a5282db2e95072405cdf4b5dfd
Alex <17277800+alex-schaaf@users.noreply.github.com>
2023-02-07 23:24:19 +0100

    improve: remove loading flicker from activity calendar

--- apps/frontend/src/components/ActivitiesCalendarContainer/ActivitiesCalendarContainer.tsx
+++ apps/frontend/src/components/ActivitiesCalendarContainer/ActivitiesCalendarContainer.tsx
@@ -3,17 +3,11 @@ import dayjs from "dayjs"
 
 import ActivitiesCalendar from "@/components/ActivitiesCalendar/ActivitiesCalendar"
 import useActivities from "@/hooks/useActivities"
-import Loading from "@/components/ui/Spinner"
 
 const ActivitiesCalendarContainer: React.FC = () => {
   const [month, setMonth] = useState(dayjs().startOf("month"))
 
-  const { activities, isLoading, isError } = useActivities(
-    month.subtract(1, "week").toDate()
-  )
-
-  if (isLoading) return <Loading />
-  if (isError) return <div>failed to fetch</div>
+  const { activities } = useActivities(month.subtract(1, "week").toDate())
 
   return (
     <ActivitiesCalendar
