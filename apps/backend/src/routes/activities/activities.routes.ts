0fd959ef2594082b14f66b06223233eb0a48bbdf
Alex <17277800+alex-schaaf@users.noreply.github.com>
2023-02-08 16:25:02 +0100

    refactor: use strava-types in backend

--- apps/backend/src/routes/activities/activities.routes.ts
+++ apps/backend/src/routes/activities/activities.routes.ts
@@ -2,6 +2,7 @@ import express, { Request } from "express";
 
 import stravaActivityService from "../../services/stravaActivityService";
 import { Prisma, StravaActivities } from "@prisma/client";
+import { SummaryActivity } from "strava-types";
 
 const router = express.Router();
 
@@ -39,60 +40,58 @@ router.get(
   }
 );
 
-router.post("/users/:userId/activities", async (req, res, next) => {
-  const { userId } = req.params;
-  const data = req.body;
+router.post(
+  "/users/:userId/activities",
+  async (
+    req: Request<{ userId: string }, {}, SummaryActivity[]>,
+    res,
+    next
+  ) => {
+    const { userId } = req.params;
+    const data = req.body;
 
-  if (Array.isArray(data)) {
-    // const objs = data.map((d) => Activity.parse(d));
-
-    const activities: StravaActivities[] = [];
-    data.forEach(async (obj) => {
+    if (Array.isArray(data)) {
+      const activities: StravaActivities[] = [];
+      data.forEach(async (obj) => {
+        try {
+          const act = await stravaActivityService.create(parseInt(userId), obj);
+          activities.push(act);
+        } catch (e) {}
+      });
+      return res.status(201).json(activities);
+    } else {
       try {
-        const act = await stravaActivityService.create(parseInt(userId), obj);
-        activities.push(act);
+        const activity = await stravaActivityService.create(
+          parseInt(userId),
+          data
+        );
+        res.status(201);
+        return res.json(activity);
       } catch (e) {
-        // console.error(e);
-      }
-    });
-    return res.status(201).json(activities);
-  } else {
-    // const obj = Activity.parse(data);
-
-    try {
-      const activity = await stravaActivityService.create(
-        parseInt(userId),
-        data
-      );
-      res.status(201);
-      return res.json(activity);
-    } catch (e) {
-      if (e instanceof Prisma.PrismaClientKnownRequestError) {
-        if (e.code === "P2002") {
-          console.error(
-            "There is a unique constraint violation, a new activity cannot be created with this id"
-          );
-          res.status(409).send(e);
+        if (e instanceof Prisma.PrismaClientKnownRequestError) {
+          if (e.code === "P2002") {
+            console.error(
+              "There is a unique constraint violation, a new activity cannot be created with this id"
+            );
+            res.status(409).send(e);
+          } else {
+            next(e);
+          }
         } else {
           next(e);
         }
-      } else {
-        next(e);
       }
     }
   }
-});
-
-router.delete(
-  "/users/:userId/activities/:activityId",
-  async (req, res, next) => {
-    const { activityId } = req.params;
-    const activity = await stravaActivityService.delete({
-      id: parseInt(activityId),
-    });
-
-    res.status(200).json(activity);
-  }
 );
+
+router.delete("/users/:userId/activities/:activityId", async (req, res) => {
+  const { activityId } = req.params;
+  const activity = await stravaActivityService.delete({
+    id: parseInt(activityId),
+  });
+
+  res.status(200).json(activity);
+});
 
 export default router;
