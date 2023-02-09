0984e2c054fa6de83c3f17f41e0d6bde0bec7f14
Alex <17277800+alex-schaaf@users.noreply.github.com>
2023-02-09 15:19:28 +0100

    config: use esbuild for dev/build

--- apps/backend/src/services/userService/UserService.test.ts
+++ apps/backend/src/services/userService/UserService.test.ts
@@ -2,5 +2,7 @@ import userService from "./UserService";
 
 test("userService.find() does not return passwordHash", async () => {
   const user = await userService.find({ id: 4 });
-  expect(user.passwordHash).toBeUndefined();
+  if (user) {
+    expect(user.passwordHash).toBeUndefined();
+  }
 });
