b39200ca177312dd506ba46f6573c85289141d44
Alex <17277800+alex-schaaf@users.noreply.github.com>
2023-02-10 11:01:54 +0100

    dx: add basic msw request mocking

---
+++ apps/frontend/src/mocks/handlers.ts
@@ -0,0 +1,19 @@
+import dayjs from "dayjs"
+import { rest } from "msw"
+import { generateActivities } from "./data/activities"
+import settings from "./data/settings"
+import user from "./data/user"
+
+const activities = generateActivities(dayjs().subtract(2, "year"))
+
+export const handlers = [
+  rest.get("*/api/v1/users", (req, res, ctx) => {
+    return res(ctx.json(user))
+  }),
+  rest.get("*/api/v1/users/*/settings/*", (req, res, ctx) => {
+    return res(ctx.json(settings))
+  }),
+  rest.get("*/api/v1/users/*/activities*", (req, res, ctx) => {
+    return res(ctx.json(activities))
+  }),
+]
