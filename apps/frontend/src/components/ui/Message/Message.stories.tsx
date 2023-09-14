5b3965604515db318ff28369919674a64209abf6
Alex <17277800+alex-schaaf@users.noreply.github.com>
2023-09-14 11:30:27 +0200

    add Message story

---
+++ apps/frontend/src/components/ui/Message/Message.stories.tsx
@@ -0,0 +1,20 @@
+// Button.stories.ts|tsx
+
+import type { Meta, StoryObj } from "@storybook/react"
+
+import Message from "./Message"
+
+const meta: Meta<typeof Message> = {
+  component: Message,
+}
+
+export default meta
+type Story = StoryObj<typeof Message>
+
+export const Success: Story = {
+  render: () => <Message text="Text" intent="success" />,
+}
+
+export const Error: Story = {
+  render: () => <Message text="Toital Distance" intent="error" />,
+}
