027d7107bb90145032bc8b7be7fabea2238762b0
Alex <17277800+alex-schaaf@users.noreply.github.com>
2024-03-21 12:33:40 +0100

    auth: increase token expiry time to 7 days

--- apps/api/src/auth/auth.module.ts
+++ apps/api/src/auth/auth.module.ts
@@ -13,7 +13,7 @@ import { AuthService } from "./auth.service"
     JwtModule.register({
       global: true,
       secret: process.env.JWT_SECRET,
-      signOptions: { expiresIn: "60s" },
+      signOptions: { expiresIn: "7 days" },
     }),
   ],
   controllers: [AuthController],
