f2ee04a13f0dd425cafc68773d904116cc98f65f
Alex <17277800+alex-schaaf@users.noreply.github.com>
2024-03-23 14:21:41 +0100

    ui: add border to TrendBadge

--- apps/frontend/src/components/ui/TrendBadge/TrendBadge.tsx
+++ apps/frontend/src/components/ui/TrendBadge/TrendBadge.tsx
@@ -16,11 +16,16 @@ const TrendBadge: React.FC<TrendBadgeProps> = ({
   unit,
   precision = 1,
 }) => {
-  const style = "text-sm px-2 py-1 rounded-sm font-medium flex gap-1"
+  const style = "text-sm px-2 py-1 rounded-sm font-medium flex gap-1 border"
 
   if (current === previous) {
     return (
-      <span className={classNames(style, "bg-warning-50 text-warning-500")}>
+      <span
+        className={classNames(
+          style,
+          "bg-warning-50 text-warning-500 border-warning-200"
+        )}
+      >
         <span>-</span>
         <span>{unit}</span>
       </span>
@@ -28,7 +33,12 @@ const TrendBadge: React.FC<TrendBadgeProps> = ({
   } else if (current > previous) {
     const ratio = current - previous
     return (
-      <span className={classNames(style, "bg-success-50 text-success-500")}>
+      <span
+        className={classNames(
+          style,
+          "bg-success-50 text-success-500 border border-success-200"
+        )}
+      >
         <span>{arrowUp}</span>
         <span>{ratio.toFixed(precision)}</span>
         <span>{unit}</span>
@@ -37,7 +47,12 @@ const TrendBadge: React.FC<TrendBadgeProps> = ({
   } else {
     const ratio = previous - current
     return (
-      <span className={classNames(style, "bg-danger-50 text-danger-500")}>
+      <span
+        className={classNames(
+          style,
+          "bg-danger-50 text-danger-500 border-danger-200"
+        )}
+      >
         <span>{arrowDown}</span>
         <span>{ratio.toFixed(precision)}</span>
         <span>{unit}</span>
