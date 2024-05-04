2d71cdc0cd54267c3e19a7ba3d7c37694a4005b9
Alex <17277800+alex-schaaf@users.noreply.github.com>
2024-05-04 21:54:32 +0200

    refactor: remove unused SignIn test file

--- apps/web/src/components/SignIn/SignIn.test.tsx
+++
@@ -1,9 +0,0 @@
-import SignIn from "./SignIn"
-import "@testing-library/jest-dom"
-import { render, screen } from "@testing-library/react"
-
-test("", async () => {
-  // ARRANGE
-  // ACT
-  // ASSERT
-})
