4feadd0432865d1dc3e66bd609890aea1b4433dd
Alex <17277800+alex-schaaf@users.noreply.github.com>
2024-05-18 16:49:24 +0200

    basic routing

---
+++ apps/web2/src/Layout.tsx
@@ -0,0 +1,7 @@
+import { useEffect } from "react"
+
+const Layout = () => {
+  return <></>
+}
+
+export default Layout
---
+++ apps/web2/src/pages/Activites.page.tsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+const ActivitiesPage = () => {
+  return <div>ActivitiesPage</div>
+}
+
+export default ActivitiesPage
