42bf76a2999665ff9f020301e1a4a3d52525e005
Alex <17277800+alex-schaaf@users.noreply.github.com>
2024-05-18 16:49:24 +0200

    basic routing

--- apps/web2/src/App.tsx
+++ apps/web2/src/App.tsx
@@ -1,10 +1,89 @@
 import "@mantine/core/styles.css"
 import "./App.css"
 
-import { MantineProvider } from "@mantine/core"
+import { AppShell, Burger, MantineProvider, NavLink } from "@mantine/core"
+import { useDisclosure } from "@mantine/hooks"
+import { RouterProvider, createBrowserRouter } from "react-router-dom"
+import DashboardPage from "./pages/Dashboard.page"
+import ActivitiesPage from "./pages/Activites.page"
+import SettingsPage from "./pages/Settings.page"
+import SignInPage from "./pages/SignIn.page"
+import SignUpPage from "./pages/SignUp.page"
+import { Outlet } from "react-router-dom"
+import { NavLink as NavLinkRouter } from "react-router-dom"
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: Layout,
+    children: [
+      {
+        index: true,
+        Component: DashboardPage,
+      },
+      {
+        path: "/dashboard",
+        Component: DashboardPage,
+      },
+      {
+        path: "/activities",
+        Component: ActivitiesPage,
+      },
+      {
+        path: "/settings",
+        Component: SettingsPage,
+      },
+      // {
+      //   path: "/auth/strava",
+      //   Component: StravaOAuthRedirect,
+      // },
+    ],
+  },
+  {
+    path: "/sign-in",
+    Component: SignInPage,
+  },
+  {
+    path: "/sign-up",
+    Component: SignUpPage,
+  },
+])
+
+function Layout() {
+  const [opened, { toggle }] = useDisclosure()
+  return (
+    <MantineProvider defaultColorScheme="dark">
+      <AppShell
+        header={{ height: 60 }}
+        navbar={{
+          width: 300,
+          breakpoint: "sm",
+          collapsed: { mobile: !opened },
+        }}
+        padding="md"
+      >
+        <AppShell.Header>
+          <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
+        </AppShell.Header>
+        <AppShell.Navbar p="md">
+          <NavLink to="/" label="Dashboard" component={NavLinkRouter} />
+          <NavLink
+            to="/activities"
+            label="Activities"
+            component={NavLinkRouter}
+          />
+          <NavLink to="/settings" label="Settings" component={NavLinkRouter} />
+        </AppShell.Navbar>
+        <AppShell.Main>
+          <Outlet />
+        </AppShell.Main>
+      </AppShell>
+    </MantineProvider>
+  )
+}
 
 function App() {
-  return <MantineProvider>🍋</MantineProvider>
+  return <RouterProvider router={router}></RouterProvider>
 }
 
 export default App
