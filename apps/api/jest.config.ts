69157c2543d2d0faed4c3bd1a64689b3d1905546
Alex <17277800+alex-schaaf@users.noreply.github.com>
2024-05-19 19:02:53 +0200

    refactor: Update jest.config.ts rootDir to use relative path

--- apps/api/jest.config.ts
+++ apps/api/jest.config.ts
@@ -1,11 +1,12 @@
-import path from "path"
+// import path from "path"
 import type { JestConfigWithTsJest } from "ts-jest"
 
 const config: JestConfigWithTsJest = {
   setupFiles: ["<rootDir>/../test/setTestEnvVars.ts"],
   moduleFileExtensions: ["js", "json", "ts"],
   preset: "ts-jest",
-  rootDir: path.resolve(__dirname, "src"),
+  // rootDir: path.resolve(__dirname, "src"),
+  rootDir: "src",
   testRegex: ".*\\.spec\\.ts$",
   transform: {
     "^.+\\.(t|j)s$": "ts-jest",
