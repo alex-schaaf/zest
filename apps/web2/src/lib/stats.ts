b67b322fe47c36479b40bbe56545c1a92adbd3fc
Alex <17277800+alex-schaaf@users.noreply.github.com>
2024-05-19 15:35:08 +0200

    feat: Calculate activity statistics in calcActivityStats function

---
+++ apps/web2/src/lib/stats.ts
@@ -0,0 +1,38 @@
+import { Activity } from "@/types/activity.types"
+
+export interface ActivityStats {
+  totalDistance: number
+  totalTime: number
+  totalElevation: number
+  averageSpeed: number
+  averageHeartrate: number
+}
+
+export const calcActivityStats = (activities: Activity[]) => {
+  const totalDistance = activities.reduce(
+    (prev, curr) => (prev += curr.distance / 1000),
+    0
+  )
+  const totalTime = activities.reduce(
+    (prev, curr) => (prev += curr.time / 60),
+    0
+  )
+  const totalElevation = activities.reduce(
+    (prev, curr) => (prev += curr.elevationGain),
+    0
+  )
+  const averageHeartrate =
+    activities.reduce((prev, curr) => (prev += curr.averageHeartrate || 0), 0) /
+    activities.filter((activity) => activity.averageHeartrate != null).length
+  const averageSpeed =
+    activities.reduce((prev, curr) => (prev += curr.speed), 0) /
+    activities.length
+
+  return {
+    totalDistance,
+    totalTime,
+    totalElevation,
+    averageSpeed,
+    averageHeartrate,
+  }
+}
