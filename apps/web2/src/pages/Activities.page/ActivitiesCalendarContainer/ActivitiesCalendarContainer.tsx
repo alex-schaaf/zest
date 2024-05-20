760d6960d91a99a6135b811fe58dc1d3faf8b7f3
Alex <17277800+alex-schaaf@users.noreply.github.com>
2024-05-20 12:25:10 +0200

    fear: add basic activities calendar

---
+++ apps/web2/src/pages/Activities.page/ActivitiesCalendarContainer/ActivitiesCalendarContainer.tsx
@@ -0,0 +1,37 @@
+import React from "react"
+import { useSearchParams } from "react-router-dom"
+import useActivities from "@/hooks/useActivities"
+import dayjs from "dayjs"
+import ActivitiesCalendar from "@/pages/Activities.page/ActivitiesCalendar"
+
+interface ActivitiesCalendarProps {}
+
+const ActivitiesCalendarContainer: React.FC<ActivitiesCalendarProps> = () => {
+  const [searchParams, _] = useSearchParams()
+
+  console.log(dayjs(searchParams.get("date")).subtract(1, "month").toDate())
+
+  const { activities, isLoading, isError } = useActivities({
+    startDateGte: dayjs(searchParams.get("date")).subtract(1, "month").toDate(),
+  })
+
+  if (isLoading) {
+    return <div>Loading...</div>
+  }
+
+  if (isError) {
+    return <div>Error...</div>
+  }
+
+  if (!activities) {
+    return <div>No activities found...</div>
+  }
+
+  return (
+    <div>
+      <ActivitiesCalendar activities={activities} />
+    </div>
+  )
+}
+
+export default ActivitiesCalendarContainer
