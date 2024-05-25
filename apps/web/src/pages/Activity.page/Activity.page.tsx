d9ad7e94eb97ab4bb602553e37f331f44cc8640b
Alex <17277800+alex-schaaf@users.noreply.github.com>
2024-05-25 22:49:04 +0200

    add value fields to activity page

--- apps/web/src/pages/Activity.page/Activity.page.tsx
+++ apps/web/src/pages/Activity.page/Activity.page.tsx
@@ -3,8 +3,11 @@ import { useParams } from "react-router-dom"
 
 import axios from "@/lib/axios"
 import { useUserContext } from "@/contexts/user-context"
-import { Paper } from "@mantine/core"
+import { Group, Paper, SimpleGrid, Stack, Text } from "@mantine/core"
 import { Activity } from "@/types/activity.types"
+import { IconRun } from "@tabler/icons-react"
+import classes from "./Activity.page.module.css"
+import dayjs from "dayjs"
 
 const ActivityPage = () => {
   const { user } = useUserContext()
@@ -31,10 +34,57 @@ const ActivityPage = () => {
   }
 
   return (
-    <Paper withBorder p="md">
-      <pre>{JSON.stringify(activity, null, 2)}</pre>
-    </Paper>
+    <Stack p="md">
+      <Group>
+        <IconRun />
+        <Text size="sm">{activity.id.toString()}</Text>
+        <Text size="sm">
+          {dayjs(activity.startDate).format("YYYY-MM-DD HH:mm")}
+        </Text>
+      </Group>
+      <SimpleGrid cols={4}>
+        <ValueCard
+          title="Distance"
+          value={(activity.distance / 1000).toFixed(1)}
+          unit="km"
+        />
+        <ValueCard
+          title="Duration"
+          value={dayjs.duration(activity.time, "seconds").format("HH:mm:ss")}
+        />
+        <ValueCard
+          title="Pace"
+          value={(1000 / activity.speed / 60).toFixed(1)}
+        />
+        <ValueCard
+          title="Elevation Gain"
+          value={activity.elevationGain.toFixed(0)}
+          unit="m"
+        />
+      </SimpleGrid>
+    </Stack>
   )
 }
 
 export default ActivityPage
+
+interface ValueCardProps {
+  title: string
+  value: string
+  unit?: string
+}
+
+const ValueCard: React.FC<ValueCardProps> = (props) => {
+  return (
+    <Paper withBorder p="md" ta="center">
+      <Stack>
+        <Text size="xs" c="dimmed" className={classes.title}>
+          {props.title}
+        </Text>
+        <Text className={classes.value}>
+          {props.value} {props.unit}
+        </Text>
+      </Stack>
+    </Paper>
+  )
+}
